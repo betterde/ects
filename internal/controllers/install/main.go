e5f49c85d4b7e62dcb79fa971fe5ef0e0e93d903
George <george@betterde.com>
2019-03-18 18:16:35 +0800

    complete install config api logic

--- internal/controllers/install/main.go
+++ internal/controllers/install/main.go
@@ -2,6 +2,7 @@ package install
 
 import (
 	"github.com/betterde/ects/config"
+	"github.com/betterde/ects/internal/models"
 	"github.com/betterde/ects/internal/utils/response"
 	"github.com/betterde/ects/internal/utils/system"
 	"github.com/kataras/iris"
@@ -10,7 +11,13 @@ import (
 )
 
 type (
-	InstallationController struct{
+	InstallationController struct {
+	}
+	Admin struct {
+		Name    string `json:"name" validate:"required"`
+		Email   string `json:"email" validate:"required"`
+		Pass    string `json:"pass" validate:"required"`
+		Confirm string `json:"confirm" validate:"required"`
 	}
 )
 
@@ -24,32 +31,64 @@ func (instance *InstallationController) Get(ctx iris.Context) {
 
 // 根据用户填写配置数据生成配置文件
 func (instance *InstallationController) Post(ctx iris.Context) {
-	var conf config.Config
-
+	var (
+		admim Admin
+		err error
+	)
 	validate := validator.New()
 
-	if err := ctx.ReadJSON(&conf); err != nil {
+	if err := ctx.ReadJSON(&config.Conf); err != nil {
 		// TODO Add logger
 	}
 
-	if err := validate.Struct(conf); err != nil {
-		if _, err := ctx.JSON(response.ValidationError("用户名和密码不能为空")); err != nil {
+	if err := ctx.ReadJSON(&admim); err != nil {
+		// TODO Add logger
+	}
+
+	if err := validate.Struct(config.Conf); err != nil {
+		if _, err := ctx.JSON(response.ValidationError("请检查配置信息是否填写完整")); err != nil {
 			// TODO Add logger
 		}
 		return
 	}
 
-	content, err := yaml.Marshal(&conf)
+	if err := validate.Struct(config.Conf); err != nil {
+		if _, err := ctx.JSON(response.ValidationError("请检查管理员用户信息是否完整")); err != nil {
+			// TODO Add logger
+		}
+		return
+	}
+
+	if models.Engine == nil {
+		// 初始化数据库
+		models.Engine, err = models.Connection()
+
+		if err != nil {
+			if _, err = ctx.JSON(response.Success("数据库连接错误", err)); err != nil {
+				// TODO
+			}
+			return
+		}
+
+		// 迁移数据表时出现错误则返回异常
+		if err := models.Migrate(); err != nil {
+			if _, err = ctx.JSON(response.Send(1045, "数据库连接错误", err)); err != nil {
+				// TODO
+			}
+			return
+		}
+	}
+
+	content, err := yaml.Marshal(&config.Conf)
 
 	if err != nil {
 		// TODO
 	}
 
+	// 将配置写入文件
 	config.WriteConfigToFile(config.Path, content)
-
-	_, err = ctx.JSON(response.Success("请求成功", system.Info))
-
-	if err != nil {
+	system.Info.Installed = true
+	if _, err = ctx.JSON(response.Success("安装成功", system.Info)); err != nil {
 		// TODO
 	}
 }
