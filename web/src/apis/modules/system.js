69c7875dd0f9b675548e3f246a08710820619c76
George <george@betterde.com>
2019-03-18 18:14:08 +0800

    add install api

--- web/src/apis/modules/system.js
+++ web/src/apis/modules/system.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 
 export default {
-  // 获取系统信息
+  /**
+   * 获取系统信息
+   * @returns {*}
+   */
   fetch () {
-    return Vue.axios.get("/api/install");
+    return Vue.axios.get('/api/install');
+  },
+  /**
+   * 创建安装配置文件
+   * @param params
+   * @returns {*}
+   */
+  install(params) {
+    return Vue.axios.post('/api/install', params)
   }
 }
