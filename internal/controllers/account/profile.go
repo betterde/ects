9be96668e91b5f303bfc3645794b812b2aae1b20
George <george@betterde.com>
2019-03-20 16:55:43 +0800

    create account profile route and controller

--- internal/controllers/account/profile.go
+++ internal/controllers/account/profile.go
@@ -1,7 +1,16 @@
 package account
 
+import (
+	"github.com/betterde/ects/internal/services"
+	"github.com/betterde/ects/internal/utils/response"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/kataras/iris"
+	"log"
+)
+
 type (
 	ProfileController struct {
+		Service services.UserService
 	}
 	Profile struct {
 		Name    string `json:"name"`
@@ -11,3 +20,30 @@ type (
 		Manager bool   `json:"manager"`
 	}
 )
+
+// 获取用户信息
+func (instance *ProfileController) Get(ctx iris.Context) {
+	token := ctx.Values().Get("jwt").(*jwt.Token)
+	claims, _ := token.Claims.(jwt.MapClaims)
+	id := claims["sub"]
+	user, err := instance.Service.FindByID(id.(string))
+	if err != nil {
+		if _, err := ctx.JSON(response.NotFound(err.Error())); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+	if _, err := ctx.JSON(response.Success("请求成功", &Profile{
+		Name:    user.Name,
+		Email:   user.Email,
+		Avatar:  user.Avatar,
+		GroupId: user.GroupId,
+		Manager: user.Manager,
+	})); err != nil {
+		log.Println(err)
+	}
+}
+
+func (instance *ProfileController) Post(ctx iris.Context) {
+
+}
