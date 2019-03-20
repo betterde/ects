ff99e7b0685e3410dd1b8a9cd816b51d2cef42d2
George <george@betterde.com>
2019-03-20 16:56:07 +0800

    modify user table

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,25 +5,25 @@ import (
 )
 
 type User struct {
-	ID        string `json:"id" xorm:"char(36) notnull unique 'id'"`
+	ID        string `json:"id" xorm:"pk char(36) notnull 'id'"`
 	Name      string `json:"name" xorm:"varchar(32) notnull"`
 	Email     string `json:"email" xorm:"varchar(64) notnull unique"`
 	Password  string `json:"-" xorm:"varchar(128) notnull"`
 	Avatar    string `json:"avatar" xorm:"varchar(255) notnull default ''"`
 	GroupId   int64  `json:"group_id" xorm:"varchar(36) int index"`
-	Manager   bool	`json:"manager" xorm:"tinyint notnull default 0"`
+	Manager   bool   `json:"manager" xorm:"tinyint notnull default 0"`
 	CreatedAt string `json:"created_at" xorm:"datetime notnull created"`
 	UpdatedAt string `json:"updated_at" xorm:"datetime null updated"`
 	DeletedAt string `json:"deleted_at" xorm:"datetime null deleted"`
 	Model     `json:"-" xorm:"-"`
 }
 
-func GeneratePassword(userPassword string) ([]byte, error) {
-	return bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+func GeneratePassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
 
-func ValidatePassword(userPassword string, hashed []byte) (bool, error) {
-	if err := bcrypt.CompareHashAndPassword(hashed, []byte(userPassword)); err != nil {
+func ValidatePassword(password string, hashed []byte) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword(hashed, []byte(password)); err != nil {
 		return false, err
 	}
 	return true, nil
@@ -31,4 +31,15 @@ func ValidatePassword(userPassword string, hashed []byte) (bool, error) {
 
 func (user *User) Store() *User {
 	return user
+}
+
+func (user *User) Save() error {
+	_, err := Engine.Id(user.ID).Update(&user)
+	return err
+}
+
+func (user *User) ModifyEmail(email string) (*User, error) {
+	user.Email = email
+	err := user.Save()
+	return user, err
 }
