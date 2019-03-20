16cd66d9bc2e5987b22f9235099276ffe432cdb3
George <george@betterde.com>
2019-03-20 18:37:48 +0800

    complete delete user logic

--- internal/services/user.go
+++ internal/services/user.go
@@ -14,6 +14,7 @@ type UserService interface {
 	Users() []models.User
 	FindByID(id string) (*models.User, error)
 	Attempt(username, password string) (string, error)
+	Destroy(id string, force bool) error
 }
 
 func NewUserService() UserService {
@@ -30,6 +31,11 @@ func (service *userService) Users() []models.User {
 // 验证用户凭证
 func (service *userService) Attempt(username, passwod string) (token string, err error) {
 	user, err := service.RetrieveByCredentials(username, passwod)
+
+	if err != nil {
+		return "", err
+	}
+
 	token, err = IssueToken(user)
 	return token, err
 }
@@ -37,6 +43,11 @@ func (service *userService) Attempt(username, passwod string) (token string, err
 // 根据用户凭证获取用户模型
 func (service *userService) RetrieveByCredentials(username, password string) (user *models.User, err error) {
 	user, err = service.FindByEmail(username)
+
+	if err != nil {
+		return nil, err
+	}
+
 	result, err := models.ValidatePassword(password, []byte(user.Password))
 
 	if result {
@@ -47,9 +58,19 @@ func (service *userService) RetrieveByCredentials(username, password string) (us
 }
 
 // 根据用户邮箱查询用户信息
-func (service *userService) FindByEmail(email string) (user *models.User, err error) {
-	_, err = models.Engine.Where(builder.Eq{"email": email}).Get(user)
-	return
+func (service *userService) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	result, err := models.Engine.Unscoped().Where(builder.Eq{"email": email}).Get(&user)
+
+	if result {
+		// 如果用户已经被删除则
+		if user.DeletedAt != "" {
+			return &user, errors.New("用户已禁用")
+		}
+		return &user, err
+	}
+
+	return &user, errors.New("用户不存在")
 }
 
 // 根据用户ID获取用户信息
@@ -65,6 +86,22 @@ func (service *userService) FindByID(id string) (*models.User, error) {
 	}
 
 	return &user, errors.New("用户不存在")
+}
+
+// 删除用户信息
+func (service *userService) Destroy(id string, force bool) (err error) {
+	var result int64
+	if force {
+		result, err = models.Engine.Id(id).Unscoped().Delete(&models.User{})
+	} else {
+		result, err = models.Engine.Id(id).Delete(&models.User{})
+	}
+
+	if err != nil && result > 0 {
+		return nil
+	}
+
+	return errors.New("用户删除失败")
 }
 
 // 为用户颁发Access Token
