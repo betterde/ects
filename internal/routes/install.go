8081844d9b94569d54191dd0de4402d45b6440a5
George <george@betterde.com>
2019-03-20 16:56:45 +0800

    remove service

--- internal/routes/install.go
+++ internal/routes/install.go
@@ -2,12 +2,9 @@ package routes
 
 import (
 	"github.com/betterde/ects/internal/controllers/install"
-	"github.com/betterde/ects/internal/services"
 	"github.com/kataras/iris/mvc"
 )
 
 func installation(application *mvc.Application) {
-	installService := services.NewInstallService()
-	application.Register(installService)
 	application.Handle(new(install.InstallationController))
 }
