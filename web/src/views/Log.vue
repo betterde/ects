bf19d5f1f1c7c33498ad0bb4b8003cdfb366d794
George <george@betterde.com>
2019-03-23 09:50:30 +0800

    create log and notification view

---
+++ web/src/views/Log.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Log"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/views/Notification.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Notification"
+    }
+</script>
+
+<style scoped>
+
+</style>
