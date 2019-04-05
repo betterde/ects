c27bead9fcf3f82c38548efc93ab92350ca520c5
George <george@betterde.com>
2019-04-06 00:08:10 +0800

    create validation error message

---
+++ internal/message/main.go
@@ -0,0 +1,19 @@
+package message
+
+import (
+	"gopkg.in/go-playground/validator.v9"
+)
+
+var (
+	modules = map[string]map[string]map[string]string{
+		"task": taskMessage(),
+	}
+)
+
+// 获取制定模块表单验证的单条消息
+func Get(module string, validationErrors validator.ValidationErrors) string {
+	first := validationErrors[0]
+	field := first.Field()
+	condition := first.Tag()
+	return modules[module][field][condition]
+}
