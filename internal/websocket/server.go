735f70054366903c96cab99f762f106f0229eaca
George <george@betterde.com>
2019-04-08 18:43:02 +0800

    modify worker attribute

--- internal/websocket/server.go
+++ internal/websocket/server.go
@@ -129,7 +129,7 @@ func (s *Server) Upgrade(ctx context.Context) Connection {
 		}
 	}
 
-	worker.IP = ctx.RemoteAddr()
+	worker.Host = ctx.RemoteAddr()
 	if err := worker.Update(); err != nil {
 		if _, err := ctx.JSON(response.NotFound(err.Error())); err != nil {
 
@@ -143,7 +143,7 @@ func (s *Server) Upgrade(ctx context.Context) Connection {
 		return &connection{err: err}
 	}
 
-	return s.handleConnection(ctx, conn, worker.ID)
+	return s.handleConnection(ctx, conn, worker.Id)
 }
 
 func (s *Server) addConnection(c *connection) {
--- models/worker.go
+++ models/worker.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 const (
 	STATUS_CONNECTED    = "connected"
 	STATUS_DISCONNECTED = "disconnected"
@@ -7,16 +9,21 @@ const (
 
 type (
 	Worker struct {
-		ID        string `json:"id" xorm:"pk char(36) notnull 'id'"`
-		Name      string `json:"name" xorm:"varchar(255) notnull 'name'"`
-		IP        string `json:"ip" xorm:"varchar(15) null 'ip'"`
-		Status    string `json:"status" xorm:"-"`
-		Remark    string `json:"remark" xorm:"varchar(255) null 'remark'"`
-		TeamID    string `json:"team_id" xorm:"char(36) notnull"`
-		CreatedAt string `json:"created_at" xorm:"datetime notnull created"`
-		UpdatedAt string `json:"updated_at" xorm:"datetime notnull updated"`
+		Id          string    `xorm:"not null pk comment('用户ID') CHAR(36)"`
+		Name        string    `xorm:"not null comment('名称') VARCHAR(255)"`
+		Host        string    `xorm:"not null comment('主机地址') VARCHAR(255)"`
+		Port        int       `xorm:"not null comment('端口') SMALLINT(5)"`
+		Status      string    `xorm:"not null default('connected') comment('状态') VARCHAR(255)"`
+		Description string    `xorm:"comment('描述') VARCHAR(255)"`
+		CreatedAt   time.Time `xorm:"not null created comment('创建于') TIMESTAMP"`
+		UpdatedAt   time.Time `xorm:"not null updated comment('更新于') TIMESTAMP"`
 	}
 )
+
+// 定义模型的数据表名称
+func (worker *Worker) TableName() string {
+	return "workers"
+}
 
 // 创建节点
 func (worker *Worker) Store() error {
@@ -26,6 +33,6 @@ func (worker *Worker) Store() error {
 
 // 更新节点信息
 func (worker *Worker) Update() error {
-	_, err := Engine.Id(worker.ID).Update(worker)
+	_, err := Engine.Id(worker.Id).Update(worker)
 	return err
 }
