2db55341a96304ec2bfba28afed94ed540281182
George <george@betterde.com>
2019-08-28 16:19:28 +0800

    completed query log api logic

--- controllers/log/main.go
+++ controllers/log/main.go
@@ -24,26 +24,70 @@ func (instance *Controller) Get(ctx iris.Context) mvc.Response {
 		err    error
 	)
 
+	scene := ctx.URLParamDefault("scene", "pipeline")
 	search := ctx.URLParamDefault("search", "")
 	page, limit, start := utils.Pagination(ctx)
-	logs := make([]models.Log, 0)
 
-	if search == "" {
-		total, err = models.Engine.Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+	switch scene {
+	case "pipeline":
+		logs := make([]models.PipelineRecords, 0)
+		if search != "" {
+			total, err = models.Engine.Where(builder.Like{"pipeline_id", search}).Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		} else {
+			total, err = models.Engine.Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		}
 
-	} else {
-		total, err = models.Engine.Where(builder.Eq{"user_id": search}).Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		if err != nil {
+			return response.InternalServerError("获取流水线日志失败", err)
+		}
+
+		return response.Success("请求成功", response.Payload{
+			"data": logs,
+			"meta": &response.Meta{
+				Limit: limit,
+				Page:  page,
+				Total: int(total),
+			},
+		})
+	case "task":
+		logs := make([]models.TaskRecords, 0)
+		if search != "" {
+			total, err = models.Engine.Where(builder.Like{"pipeline_id", search}).Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		} else {
+			total, err = models.Engine.Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		}
+
+		if err != nil {
+			return response.InternalServerError("获取任务日志失败", err)
+		}
+
+		return response.Success("请求成功", response.Payload{
+			"data": logs,
+			"meta": &response.Meta{
+				Limit: limit,
+				Page:  page,
+				Total: int(total),
+			},
+		})
+	case "user":
+		logs := make([]models.Log, 0)
+		if search == "" {
+			total, err = models.Engine.Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		} else {
+			total, err = models.Engine.Where(builder.Eq{"user_id": search}).Limit(limit, start).Desc("created_at").FindAndCount(&logs)
+		}
+
+		if err != nil {
+			log.Println(err)
+		}
+		return response.Success("Success", response.Payload{
+			"data": logs,
+			"meta": &response.Meta{
+				Limit: limit,
+				Page:  page,
+				Total: int(total),
+			}})
 	}
 
-	if err != nil {
-		log.Println(err)
-	}
-
-	return response.Success("Success", response.Payload{
-		"data": logs,
-		"meta": &response.Meta{
-			Limit: limit,
-			Page:  page,
-			Total: int(total),
-		}})
+	return response.Send(400, "请选择日志类型", make([]interface{}, 0))
 }
