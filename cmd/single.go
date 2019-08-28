9b63e84a42794040dabb1bb2938bc2ab895957f3
George <george@betterde.com>
2019-08-28 18:11:56 +0800

    delete testing code

--- cmd/single.go
+++ cmd/single.go
@@ -1,14 +1,7 @@
 package cmd
 
 import (
-	"crypto/tls"
-	"fmt"
-	"github.com/betterde/ects/models"
-	"github.com/gorhill/cronexpr"
 	"github.com/spf13/cobra"
-	"gopkg.in/gomail.v2"
-	"log"
-	"time"
 )
 
 var singleCmd = &cobra.Command{
@@ -16,21 +9,6 @@ var singleCmd = &cobra.Command{
 	Short: "Run a single node service",
 	Long:  "Run a single node service on this server",
 	Run: func(cmd *cobra.Command, args []string) {
-		//discover.NewClient()
-		//config.Conf = config.Init()
-		//discover.GetConf("/ects/config")
-		//pipeline.WatchPipelines("7df52971-4894-4f01-9171-7452c4ddceca")
-		log.Println(cronexpr.MustParse("* * * * * *").Next(time.Now()).Format(models.DefaultTimeFormat))
-		d := gomail.NewDialer("smtp.qiye.aliyun.com", 465, "system@betterde.com", "System@2019")
-		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-		m := gomail.NewMessage()
-		m.SetHeader("From", fmt.Sprintf("%s<%s>", "ECTS", "system@betterde.com"))
-		m.SetHeader("To", "george@betterde.com")
-		m.SetHeader("Subject", "Notification")
-		m.SetBody("text/html", "Hello Goerge")
-		if err := d.DialAndSend(m); err != nil {
-			log.Println(err)
-		}
 	},
 }
 
